90df8f326a9671177a6667442d1b47ebddeaba07
yasu0305 <yasuyukikaji@gmail.com>
2025-11-09 23:06:49 +0900

    test

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <format>
+#include <stdio.h>
 
 int main() {
     std::cout << "Hello, World1!" << std::endl;
+    std::cout << std::format("Hello, {}!", "World2") << std::endl;
+    std::printf("Hello, %s!\n", "World3");
+    printf("Hello, %s!\n", "World4");
     return 0;
 }
